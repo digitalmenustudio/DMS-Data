cb588bd30756df09f7cdb84a1bcf798e9a90d4ad
SimSimSim <digitalmenustudio@gmail.com>
2024-10-23 14:20:40 -0400

    add translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     include Pundit::Authorization
-
+    include NoticeHelper
+    include LocaleHelper
+    
     before_action :authenticate_user!
     after_action :verify_authorized, unless: :devise_controller?
     after_action :verify_policy_scoped, except: %i[new create]
