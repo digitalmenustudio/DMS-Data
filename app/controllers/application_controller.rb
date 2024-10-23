e6423dae7cde50bc6f33eed6b607c3f90d3e8c02
SimSimSim <digitalmenustudio@gmail.com>
2024-10-23 15:06:26 -0400

    improve sidbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,5 @@ class ApplicationController < ActionController::Base
     
     before_action :authenticate_user!
     after_action :verify_authorized, unless: :devise_controller?
-    after_action :verify_policy_scoped, except: %i[new create]
+    after_action :verify_policy_scoped, except: %i[new create destroy]
 end
