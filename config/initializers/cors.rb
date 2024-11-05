1d9eee9b297c027ea5bffc4e66c7564d5ffd94bb
SimSimSim <digitalmenustudio@gmail.com>
2024-11-04 20:54:16 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   
     # Allow requests from your deployed Next.js app in production
     allow do
-      origins 'https://your-nextjs-app.com'  # Replace with your production Next.js app domain
+      origins 'https://dms-analytic.vercel.app'  # Replace with your production Next.js app domain
       resource '*',
         headers: :any,
         methods: [:get, :options, :head],
